bc798079384a9e64d66af486428d651c5c9b8539
Quoocvuong <lanhvuong2607@gmail.com>
2025-06-11 11:56:29 +0700

    da sua loi ngay 11062025

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-domain.com/api' // URL API khi deploy lên production
+  apiUrl: 'https://your-production-api-domain.com/api', // URL API khi deploy lên production
+  wsUrl: 'ws://your-production-api-domain.com/ws',
 };
