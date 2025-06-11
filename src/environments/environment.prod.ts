42ab807bfcf6632295b88fa110e1c767796bd279
Quoocvuong <lanhvuong2607@gmail.com>
2025-06-11 19:07:10 +0700

    tao bien moi truong prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-domain.com/api', // URL API khi deploy lên production
-  wsUrl: 'ws://your-production-api-domain.com/ws',
+  // Sử dụng process.env để đọc biến môi trường do Vercel cung cấp
+  apiUrl: process.env['NG_APP_API_URL'] || 'http://your-default-fallback-api.com/api',
+  wsUrl: process.env['NG_APP_WS_URL'] || 'ws://your-default-fallback-api.com/ws',
 };
